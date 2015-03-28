aa3ad002925b7339710056ea2ffdd40fea28348c
RNO321 <arno.burnuk@gmail.com>
2015-03-28 02:00:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var express = require('express')
-var app = express()
 
+var express = require('express');
+var app = express();
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-var server = app.listen(apt.get('port'), function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(request, response) {
+response.send('Hello World!');
+});
+app.listen(app.get('port'), function() {
+console.log("Node app is running at localhost:" + app.get('port'));
+});
